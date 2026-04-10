323f1d15ffe00de6adb42013d6540002edf44498
Thriambakeshwar <thriambakeshwarsharma@gmail.com>
2026-04-10 17:29:47 +0530

    Create Thriambakeshwar_Prototype.cs

---
+++ Thriambakeshwar_Prototype.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Video Rental System Prototype");
+        Console.WriteLine("Developed by Sagar");
+
+        Console.WriteLine("1. Browse Movies");
+        Console.WriteLine("2. Rent Movie");
+        Console.WriteLine("3. Return Movie");
+
+        Console.WriteLine("Prototype version - basic functionality");
+    }
+}
